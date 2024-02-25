868839e4c3e65c9f8c42d92c6e84ffd1edbbbd2c
ShubhKeshari <shubh1999keshari@gmail.com>
2024-02-25 15:35:56 +0530

    deployement link modified

--- frontend/scripts/page.js
+++ frontend/scripts/page.js
@@ -1,16 +1,21 @@
+const isDevelopment = window.location.hostname.includes("127.0.0.1");
+let baseURL = isDevelopment
+  ? "http://127.0.0.1:5500/hack-sculptress-6789/frontend"
+  : "https://hack-sculptress-6789-1.onrender.com";
+
 function logolink() {
-  window.location.href =
-    "http://127.0.0.1:5500/hack-sculptress-6789/frontend/index.html";
+  let url = `${baseURL}/index.html`;
+  console.log(url);
+  window.location.href = `${baseURL}/index.html`;
 }
 function cartPage() {
-  window.location.href = "pages/cart.html";
+  window.location.href = `${baseURL}/pages/cart.html`;
 }
 
 function loginPage() {
-  window.location.href = "http://127.0.0.1:5501/hack-sculptress-6789/frontend/pages/login.html";
+  window.location.href = `${baseURL}/pages/login.html`;
 }
 
 function signupPage() {
-  window.location.href =
-    "http://127.0.0.1:5501/hack-sculptress-6789/frontend/pages/signup.html";
+  window.location.href = `${baseURL}/pages/signup.html`;
 }
