6ac95b3631e69d28a0e2eaaff4df8ac5bdc3ce4e
ShubhKeshari <shubh1999keshari@gmail.com>
2024-02-25 11:29:10 +0530

    pages link added

---
+++ frontend/scripts/page.js
@@ -0,0 +1,15 @@
+
+function logolink() {
+    window.location.href = "http://127.0.0.1:5500/hack-sculptress-6789/frontend/index.html";
+  }
+  function cartPage() {
+    window.location.href = "pages/cart.html";
+  }
+  
+  function loginPage() {
+    window.location.href = "pages/login.html";
+  }
+  
+  function signupPage() {
+    window.location.href = "pages/signup.html";
+  }
