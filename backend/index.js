be8d394f90c4163a7a8632872dfe81e687f3ed03
ShubhKeshari <shubh1999keshari@gmail.com>
2024-02-25 14:53:48 +0530

    server access

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const jsonServer = require("json-server");
 const auth = require("json-server-auth");
+const cors = require("cors");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
@@ -7,6 +8,7 @@ const middlewares = jsonServer.defaults();
 server.db = router.db;
 
 // Set up the authentication middleware
+server.use(cors());
 server.use(auth);
 server.use(middlewares);
 server.use(router);
--- frontend/scripts/page.js
+++ frontend/scripts/page.js
@@ -1,15 +1,16 @@
-
 function logolink() {
-    window.location.href = "http://127.0.0.1:5500/hack-sculptress-6789/frontend/index.html";
-  }
-  function cartPage() {
-    window.location.href = "pages/cart.html";
-  }
-  
-  function loginPage() {
-    window.location.href = "pages/login.html";
-  }
-  
-  function signupPage() {
-    window.location.href = "pages/signup.html";
-  }
+  window.location.href =
+    "http://127.0.0.1:5500/hack-sculptress-6789/frontend/index.html";
+}
+function cartPage() {
+  window.location.href = "pages/cart.html";
+}
+
+function loginPage() {
+  window.location.href = "http://127.0.0.1:5501/hack-sculptress-6789/frontend/pages/login.html";
+}
+
+function signupPage() {
+  window.location.href =
+    "http://127.0.0.1:5501/hack-sculptress-6789/frontend/pages/signup.html";
+}
